04895376afee34be3c7d184d35bdb98d70e3cf25
d-smith <doug.smith.mail@gmail.com>
2016-11-22 07:06:45 -0800

    Updated dumpevent to be consistent with changes to its dependencies

--- dumpevent/dumpevent.go
+++ dumpevent/dumpevent.go
@@ -1,26 +1,38 @@
 package main
 
+//This is a little test utility used for development - updated is with your credentials and import the
+//appropriate event sourced entities to dump them
+
+
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/xtracdev/goes/sample/testagg"
 	"github.com/xtracdev/oraeventstore"
 	"os"
+	"fmt"
+	"database/sql"
 )
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
 
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Usaged: go run dumpevent.go <aggregate id>")
 	}
 
-	eventStore, err := oraeventstore.NewOraEventStore("replicantusr", "password", "xe.oracle.docker", "localhost", "1521")
-	if err != nil {
-		log.Fatalf("Error instantiating oracle event store")
-	}
+	var connectStr = fmt.Sprintf("%s/%s@//%s:%s/%s", "replicantusr", "password", "localhost", "1521", "xe.oracle.docker")
+	db, err := sql.Open("oci8", connectStr)
+	fatal(err)
+
+	eventStore, err := oraeventstore.NewOraEventStore(db)
+	fatal(err)
 
 	events, err := eventStore.RetrieveEvents(os.Args[1])
-	if err != nil {
-		log.Fatalf("Error loading events: %s", err.Error())
-	}
+	fatal(err)
 
 	testAgg := testagg.NewTestAggFromHistory(events)
 	if testAgg == nil {
@@ -29,7 +41,7 @@ func main() {
 	}
 
 	log.Infof("Your aggregate:")
-	log.Infof("\taggregate id: %s", testAgg.Aggregate.ID)
+	log.Infof("\taggregate id: %s", testAgg.Aggregate.AggregateID)
 	log.Infof("\tversion: %d", testAgg.Version)
 	log.Infof("\tfoo: %s", testAgg.Foo)
 	log.Infof("\tbar: %s", testAgg.Bar)
